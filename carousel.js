a38d0bb7b25e7462a47dabbe041d2262e6239e23
alexisben <alexiben7@gmail.com>
2020-10-22 09:59:52 +0200

    Animate theme slider

--- carousel.js
+++ carousel.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
     $('#carousel').slick({
         dots: true,
         infinite: true,
-        arrows: true,
+        // arrows: true,
         slidesToShow: 3,
-        slidesToScroll: 3
-        // prevArrow: $('.prev')
-        // nextArrow: $('.next')   
+        slidesToScroll: 3,
+        // prevArrow: $('.prev'),
+        // nextArrow: $('.next'),
+        prevArrow: "<div>ici l'icon font awesome</div>",
+        nextArrow: "<div>ici l'icon font awesome</div>",
     });
 
 });
