e8f9241786981b8196fa079e1de12a2c0a1bd34c
Alyssalade <galydraw@gmail.com>
2020-10-22 09:05:41 +0200

    carousel, dessins, scrollbar

---
+++ carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#carousel').slick({
+        dots: true,
+        infinite: true,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+        // prevArrow: $('.prev')
+        // nextArrow: $('.next')   
+    });
+
+});
